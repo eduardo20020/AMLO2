2a6c2af908d9d1ef09ff509fac9878f024e091ad
Brandon <brandonde2002@gmail.com>
2024-04-17 07:58:14 -0600

    hola munde

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.base')
 
 @section('main')
+
+<div class="container mx-auto my-4 flex justify-center items-center">
+  <h1 class="text-4xl font-bold my-2">Nosotros</h1>
+</div>
 <div class="container mx-auto my-8 flex justify-center items-center">
   <!-- Contenido centrado vertical y horizontalmente -->
   <p class="text-lg">Somos un equipo apasionado de cinco estudiantes de la Universidad Politécnica de García, en el octavo cuatrimestre de nuestros estudios en el año 2024. Nos unimos con el propósito de crear una plataforma innovadora que facilite la conexión entre estudiantes comprometidos y organizaciones que buscan impulsar el cambio social a través del servicio comunitario. Con nuestra pasión por el impacto positivo y nuestra dedicación, estamos transformando la forma en que se lleva a cabo el servicio social.</p>
